c2cd336e1a6076135416fe30a15952dced825b33
Brady Stoffel <stoff056@umn.edu>
2020-03-30 14:49:47 -0500

    Should have all conversions

--- SmartLights.ino
+++ SmartLights.ino
@@ -9,36 +9,6 @@
 
 
 //LED Stuff
-
-#define EFFECT_NAME__0     "Strobe"
-#define EFFECT_NAME__1     "Cylon Bounce"
-#define EFFECT_NAME__2     "Fire"
-#define EFFECT_NAME__3     "Color Wipe"
-#define EFFECT_NAME__4     "Fade In and Out"
-#define EFFECT_NAME__5     "Halloween Eyes"
-#define EFFECT_NAME__6     "KITT"
-#define EFFECT_NAME__7     "Rainbow Cycle"
-#define EFFECT_NAME__8     "Cool Cycle"
-#define EFFECT_NAME__9     "Warm Cycle"
-#define EFFECT_NAME__10    "Nature Cycle"
-#define EFFECT_NAME__11    "Candy Cycle"
-#define EFFECT_NAME__12    "Christmas Cycle"
-#define EFFECT_NAME__13    "Twinkle"
-#define EFFECT_NAME__14    "Twinkle Random"
-#define EFFECT_NAME__15    "Running Lights"
-#define EFFECT_NAME__16    "Snow Sparkle"
-#define EFFECT_NAME__17    "Bouncing Balls"
-#define EFFECT_NAME__18    "Bouncing Colored Balls"
-#define EFFECT_NAME__19    "Sparkle"
-#define EFFECT_NAME__20    "Theater Chase"
-#define EFFECT_NAME__21    "Theater Chase Rainbow"
-#define EFFECT_NAME__22    "Candy Cane"
-#define EFFECT_NAME__23    "Bright White"
-#define EFFECT_NAME__24    "Off"
-#define EFFECT_NAME__25    "Mid White"
-#define EFFECT_NAME__26    "Upper Mid White"
-#define EFFECT_NAME__27    "Low White"
-
 String effectData[][28] = {{"25", "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","26", "27", "28"},
                     {"Off", "Strobe", "Cylon Bounce", "Fire", "Color Wipe", "Fade in and Out", "Halloween Eyes", "KITT", "Rainbow Cylce", 
                     "Cool Cycle", "Warm Cycle", "Nature Cycle", "Candy Cycle", "Christmas Cycle", "Twinkle", "Random Twinkle", 
@@ -213,35 +183,10 @@ void setupWemoStuff() {
     // "Alexa, turn on yellow lamp
     // "Alexa, set yellow lamp to fifty" (50 means 50% of brightness, note, this example does not use this functionality)
 
-    // Add virtual devices
-   fauxmo.addDevice(EFFECT_NAME__0);
-    fauxmo.addDevice(EFFECT_NAME__1);
-    fauxmo.addDevice(EFFECT_NAME__2);
-    fauxmo.addDevice(EFFECT_NAME__3);
-    fauxmo.addDevice(EFFECT_NAME__4);
-    fauxmo.addDevice(EFFECT_NAME__5);
-    fauxmo.addDevice(EFFECT_NAME__6);
-    fauxmo.addDevice(EFFECT_NAME__7);
-    fauxmo.addDevice(EFFECT_NAME__8);
-    fauxmo.addDevice(EFFECT_NAME__9);
-    fauxmo.addDevice(EFFECT_NAME__10);
-    fauxmo.addDevice(EFFECT_NAME__11);
-    fauxmo.addDevice(EFFECT_NAME__12);
-    fauxmo.addDevice(EFFECT_NAME__13);
-    fauxmo.addDevice(EFFECT_NAME__14);
-    fauxmo.addDevice(EFFECT_NAME__15);
-    fauxmo.addDevice(EFFECT_NAME__16);
-    fauxmo.addDevice(EFFECT_NAME__17);
-    fauxmo.addDevice(EFFECT_NAME__18);
-    fauxmo.addDevice(EFFECT_NAME__19);
-    fauxmo.addDevice(EFFECT_NAME__20);
-    fauxmo.addDevice(EFFECT_NAME__21);
-    fauxmo.addDevice(EFFECT_NAME__22);
-    fauxmo.addDevice(EFFECT_NAME__23);
-    fauxmo.addDevice(EFFECT_NAME__24);
-    fauxmo.addDevice(EFFECT_NAME__25);
-    fauxmo.addDevice(EFFECT_NAME__26);
-    fauxmo.addDevice(EFFECT_NAME__27);
+
+    for(int i = 0; i < effectDataLength; i++) {
+      fauxmo.addDevice(effectData[1][i].c_str());
+    }
     
     fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
         
@@ -256,62 +201,12 @@ void setupWemoStuff() {
         // Checking for device_id is simpler if you are certain about the order they are loaded and it does not change.
         // Otherwise comparing the device_name is safer.
 
-        if (strcmp(device_name, EFFECT_NAME__0)==0) {
-            currentCase = 1;
-        } else if (strcmp(device_name, EFFECT_NAME__1)==0) {
-            currentCase = 2;
-        } else if (strcmp(device_name, EFFECT_NAME__2)==0) {
-            currentCase = 3;
-        } else if (strcmp(device_name, EFFECT_NAME__3)==0) {
-            currentCase = 4;
-        } else if (strcmp(device_name, EFFECT_NAME__4)==0) {
-            currentCase = 5;
-        } else if (strcmp(device_name, EFFECT_NAME__5)==0) {
-            currentCase = 6;
-        } else if (strcmp(device_name, EFFECT_NAME__6)==0) {
-            currentCase = 7;
-        } else if (strcmp(device_name, EFFECT_NAME__7)==0) {
-            currentCase = 8;
-        } else if (strcmp(device_name, EFFECT_NAME__8)==0) {
-            currentCase = 9;
-        } else if (strcmp(device_name, EFFECT_NAME__9)==0) {
-            currentCase = 10;
-        } else if (strcmp(device_name, EFFECT_NAME__10)==0) {
-            currentCase = 11;
-        } else if (strcmp(device_name, EFFECT_NAME__11)==0) {
-            currentCase = 12;
-        } else if (strcmp(device_name, EFFECT_NAME__12)==0) {
-            currentCase = 13;
-        } else if (strcmp(device_name, EFFECT_NAME__13)==0) {
-            currentCase = 14;
-        } else if (strcmp(device_name, EFFECT_NAME__14)==0) {
-            currentCase = 15;
-        } else if (strcmp(device_name, EFFECT_NAME__15)==0) {
-            currentCase = 16;
-        } else if (strcmp(device_name, EFFECT_NAME__16)==0) {
-            currentCase = 17;
-        } else if (strcmp(device_name, EFFECT_NAME__17)==0) {
-            currentCase = 18;
-        } else if (strcmp(device_name, EFFECT_NAME__18)==0) {
-            currentCase = 19;
-        } else if (strcmp(device_name, EFFECT_NAME__19)==0) {
-            currentCase = 20;
-        } else if (strcmp(device_name, EFFECT_NAME__20)==0) {
-            currentCase = 21;
-        } else if (strcmp(device_name, EFFECT_NAME__21)==0) {
-            currentCase = 22;
-        } else if (strcmp(device_name, EFFECT_NAME__22)==0) {
-            currentCase = 23;
-        } else if (strcmp(device_name, EFFECT_NAME__23)==0) {
-            currentCase = 24;
-        } else if (strcmp(device_name, EFFECT_NAME__24)==0) {
-            currentCase = 25;
-        } else if (strcmp(device_name, EFFECT_NAME__25)==0) {
-            currentCase = 26;
-        } else if (strcmp(device_name, EFFECT_NAME__26)==0) {
-            currentCase = 27;
-        } else if (strcmp(device_name, EFFECT_NAME__27)==0) {
-            currentCase = 28;
+        for (int i = 0; i < effectDataLength; i++) {
+          if (strcmp(device_name, effectData[1][i].c_str())==0) {
+            currentCase = atoi(effectData[0][i].c_str());
+            break;
+          }
+          
         }
         brightness = value;
 
@@ -454,11 +349,16 @@ void baseCase() {
 void turnOnCase(int case1) {
 
   currentCase = case1;
+
+  int i = 0;
+  while(atoi(effectData[0][i].c_str()) != case1 && i < effectDataLength) {
+    i++;
+  }
   
-//  fauxmo.setState(case1, true, 255);
+  fauxmo.setState(effectData[1][i].c_str(), true, 255);
   
   Serial.print("Turning on Case: ");
-  Serial.println(case1);
+  Serial.println(effectData[1][i]);
 
   neverChanged = false;
   sendBodyResponse(true);
